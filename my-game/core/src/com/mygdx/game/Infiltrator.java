a31f7afc7b50c153cf0fd14456807b06d663b430
AlexandrePinneauHe <3121465535@qq.com>
2020-11-14 19:59:24 +0800

    Added the base inflitrator and player
    So that you can add more codes in them

---
+++ my-game/core/src/com/mygdx/game/Infiltrator.java
@@ -0,0 +1,61 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+
+public class Infiltrator extends Actor{
+    private TextureRegion region;
+
+
+    public  Infiltrator(TextureRegion region){
+        super();
+        this.region = region;
+        setSize(this.region.getRegionWidth(), this.region.getRegionHeight());
+    }
+
+
+    public TextureRegion getRegion(){
+        return region;
+    }
+
+
+    public void setRegion(TextureRegion region){
+        this.region = region;
+        setSize(this.region.getRegionWidth(), this.region.getRegionHeight());
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        if (region == null || !isVisible()){
+            return;
+        }
+
+    /* batch.draw(
+				region,
+				x, y,
+				originX, originY,
+				width, height,
+				scaleX, scaleY,
+				rotation
+		);*/
+        batch.draw(region,
+                getX(), getY(),
+                getOriginX(), getOriginY(),
+                getWidth(), getHeight(),
+                getScaleX(), getScaleY(),
+                getRotation());
+    }
+
+    public void PerformSabotage(){
+        
+    }
+}
--- my-game/core/src/com/mygdx/game/MyGdxGame.java
+++ my-game/core/src/com/mygdx/game/MyGdxGame.java
@@ -5,23 +5,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Infiltrator infiltrator;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		infiltrator = new Infiltrator(new TextureRegion(img));
+		infiltrator.setPosition(50,100);
+		infiltrator.setOrigin(0,0);
+		infiltrator.setScale(0.5F,1.0F);
+		infiltrator.setRotation(-10);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		infiltrator.act(Gdx.graphics.getDeltaTime());
 		batch.begin();
 		batch.draw(img, 0, 0);
+		infiltrator.draw(batch, 1.0F);
 		batch.end();
 	}
 	
